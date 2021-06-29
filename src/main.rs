d5ba0fff1039f021c2ab49e34fcf935774e742ba
Piyush Suthar <32237558+PiyushSuthar@users.noreply.github.com>
2021-06-29 23:02:54 +0530

    first commit

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+mod utils;
+use utils::get_data;
+
+fn main() {
+    let data = get_data("piyushsthr".to_string());
+    // let da = serde_json::to_string(&data).unwrap();
+    // let some_more_data = serde_json::json!({
+    //   "query": "query",
+    //   "variables": {
+    //     "username": "Poiyusy"
+    //   }
+    // });
+
+    let entry = data.data.media_list_collection.lists[0].entries[0];
+
+    let entry_name = entry.media.title.english.unwrap();
+
+    println!("Hello, world!, {}", &entry_name)
+}
